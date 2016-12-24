854c9978349b89574a4e0f1a5610f50cfe5a0b3e
Sachin Sawant <sachin.sawant123@gmail.com>
2016-12-24 15:24:43 +0530

    Created a new single view iPhone applications with basic configuration

---
+++ LazyLoader/LazyLoader/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LazyLoader
+//
+//  Created by Sachin Sawant on 24/12/16.
+//  Copyright © 2016 Sachin Sawant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
